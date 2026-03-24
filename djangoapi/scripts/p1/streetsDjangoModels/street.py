e1f3d18057d472f7e5d60b66b63a568fb7cbbe39
Linus Juetz <linus.juetz@students.hs-mainz.de>
2026-03-24 12:34:19 +0100

    improved insertion for django streets

--- djangoapi/scripts/p1/streetsDjangoModels/street.py
+++ djangoapi/scripts/p1/streetsDjangoModels/street.py
@@ -1,12 +1,41 @@
+from djangoapi.settings import EPSG_FOR_GEOMETRIES, ST_SNAP_PRECISION
 from erasmus_valencia.models import Street as StreetModel
 from django.forms.models import model_to_dict
 from django.contrib.gis.geos import GEOSGeometry
+from django.db import connection
+
 
 class Street:
 
+    def _intersects(self, geom:GEOSGeometry, exclude_id: int = None) -> list:
+        '''
+        Checks if the geometry intersects with any other geometry in the street table. 
+        If it intersects return a list of the ids of the intersecting streets, if it does not intersect return an empty list
+        '''
+
+        # get all the street models the django way without sql
+        streets = StreetModel.objects.all()
+        
+        # if we update a street, it must be excluded from the list to avoid self comparison
+        if exclude_id is not None:
+            streets = streets.exclude(id=exclude_id)
+        intersecting_streets = []
+        for street in streets:
+            if street.geom.intersects(geom):
+                intersecting_streets.append(street.id)
+        return intersecting_streets
+    
+    def _snap_to_grid(self, geom:GEOSGeometry) -> GEOSGeometry:
+        '''
+        Snap the geometry to a grid similiar to ST_SnapToGrid in postgis. The Code is from the profs pdf
+        '''
+        cursor=connection.cursor()
+        query="select st_snaptogrid(st_geomfromtext(%s, %s),%s)"
+        cursor.execute(query, [geom.wkt, EPSG_FOR_GEOMETRIES, ST_SNAP_PRECISION])
+
+        return GEOSGeometry(cursor.fetchall()[0][0], srid=EPSG_FOR_GEOMETRIES)
+
     def insert(self, data:dict) -> dict:
-
-
 
         try: 
             street_model = StreetModel()
@@ -18,9 +47,27 @@ class Street:
 
             street_model.visitedAt = data['visitedAt']
 
-            # check if the geom is valid, if it is not valid return an error message
-            geom = GEOSGeometry(data["geom"], srid=25830)
+            # create the geom from wkt 
+            geom = GEOSGeometry(data["geom"], srid=EPSG_FOR_GEOMETRIES)
 
+            # snap the geometry to a grid to avoid precision issues 
+            geom = self._snap_to_grid(geom)
+
+            intersection_list = self._intersects(geom)
+
+            # check if the geometry intersects with any other geometry in the street table, if it does intersect ask the user if they want to insert it anyway
+            # streets can actually intersect so its not an error, but we want to warn the user about it
+            if len(intersection_list) > 0:
+                insert = input(f"The geometry intersects with another street at id(s) {intersection_list} Do you want to insert it anyway? (y/n)") == "y"
+
+                if not insert:
+                    return {
+                        "ok": False,
+                        "message": "The geometry intersects with another street, aborted by user.",
+                        "data": []
+                    }
+
+            # check if the geom is valid, if it is not valid return an error message
             if not geom.valid:
                 return {
                     "ok": False,
@@ -55,7 +102,9 @@ class Street:
             street.visitedAt = data['visitedAt']
 
             # check if the geom is valid, if it is not valid return an error message
-            geom = GEOSGeometry(data["geom"], srid=25830)
+            geom = GEOSGeometry(data["geom"], srid=EPSG_FOR_GEOMETRIES)
+
+            geom = self._snap_to_grid(geom)
 
             if not geom.valid:
                 return {
