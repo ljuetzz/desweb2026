7b9f90779945b21f9448ef7326e46adf434e1d69
Linus Juetz <linus.juetz@students.hs-mainz.de>
2026-03-24 13:11:27 +0100

    imporved insertion for points

--- djangoapi/scripts/p1/poiDjangoModels/poi.py
+++ djangoapi/scripts/p1/poiDjangoModels/poi.py
@@ -1,9 +1,44 @@
+from django.db import connection
+
 from erasmus_valencia.models import POI as POIModel
 from django.forms.models import model_to_dict
 from django.contrib.gis.geos import GEOSGeometry
+from djangoapi.settings import EPSG_FOR_GEOMETRIES, ST_SNAP_PRECISION
+
 
 class POI:
 
+
+    def _check_identical(self, geom:GEOSGeometry, exclude_id: int = None) -> bool:
+
+        '''
+        Checks if the geometry is identical to any other geometry in the poi table.
+        This has to be done after snap_to_grid!
+        '''
+        
+        # get all the pois the django way without sql
+        pois = POIModel.objects.all()
+
+        # if we update a poi, it must be excluded from the list to avoid self comparison
+        if exclude_id is not None:
+            pois = pois.exclude(id=exclude_id)
+
+        # check for each point wether the geometry is identical. This has to be done after snap_to_grid
+        for poi in pois:
+            if poi.geom.equals(geom):
+                return True
+        return False
+
+    def _snap_to_grid(self, geom:GEOSGeometry) -> GEOSGeometry:
+        '''
+        Snap the geometry to a grid similiar to ST_SnapToGrid in postgis. The Code is from the profs pdf
+        '''
+        cursor=connection.cursor()
+        query="select st_snaptogrid(st_geomfromtext(%s, %s),%s)"
+        cursor.execute(query, [geom.wkt, EPSG_FOR_GEOMETRIES, ST_SNAP_PRECISION])
+
+        return GEOSGeometry(cursor.fetchall()[0][0], srid=EPSG_FOR_GEOMETRIES)
+    
     def insert(self, data:dict) -> dict:
         try: 
             poi_model = POIModel()
@@ -13,12 +48,22 @@ class POI:
             poi_model.visitedAt = data['visitedAt']
 
             # check if the geom is valid, if it is not valid return an error message
-            geom = GEOSGeometry(data["geom"], srid=25830)
+            geom = GEOSGeometry(data["geom"], srid=EPSG_FOR_GEOMETRIES)
+
+            # snap to grid to avoid precision issues that can cause the geometry to be invalid or to intersect with other geometries
+            geom = self._snap_to_grid(geom)
 
             if not geom.valid:
                 return {
                     "ok": False,
                     "message": "Invalid geometry",
+                    "data": []
+                }
+            
+            if self._check_identical(geom):
+                return {
+                    "ok": False,
+                    "message": "The Point is identical to another geometry in the poi table: The same point already exists!",
                     "data": []
                 }
 
@@ -44,13 +89,23 @@ class POI:
             poi.category = data['category']
             poi.visitedAt = data['visitedAt']
 
+
             # check if the geom is valid, if it is not valid return an error message
-            geom = GEOSGeometry(data["geom"], srid=25830)
+            geom = GEOSGeometry(data["geom"], srid=EPSG_FOR_GEOMETRIES)
+
+            geom = self._snap_to_grid(geom)
 
             if not geom.valid:
                 return {
                     "ok": False,
                     "message": "Invalid geometry",
+                    "data": []
+                }
+            
+            if self._check_identical(geom, exclude_id=id):
+                return {
+                    "ok": False,
+                    "message": "The Point is identical to another geometry in the poi table: The same point already exists!",
                     "data": []
                 }
 
