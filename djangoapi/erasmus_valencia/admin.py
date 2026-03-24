dbd7fafd14f7a375baf40d425ee29915b44cde53
Linus Juetz <linus.juetz@students.hs-mainz.de>
2026-03-24 20:09:38 +0100

    registered models in admin panel

--- djangoapi/erasmus_valencia/admin.py
+++ djangoapi/erasmus_valencia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Building, Street, POI
+
+admin.site.register(Building)
+admin.site.register(Street)
+admin.site.register(POI)
